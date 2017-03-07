ef3f0ab375dfd076d8aa7d9831eb75a16764e362
Mladen Marjanovic <mladen.marjanovic1992@gmail.com>
2017-03-07 20:59:37 +0000

    add user signin

---
+++ spec/features/users/user_signin_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.feature "User sign in" do
+  
+  before do 
+      @mladen = User.create!(email: "mladen@email.com", password: "password")
+      
+    end
+  
+  scenario "with valid credentials" do
+    
+    visit "/"
+    click_link("Sign in")
+    
+    fill_in "Email", with: "mladen@email.com"
+    fill_in "Password", with: "password"
+    click_button("Log in")
+    
+    expect(page).to have_content("Signed in successfully.")
+    expect(page).to have_content("Signed in as #{@mladen.email}")
+    
+  end
+  
+end
