9b09f8103b412b73646480369b709ebd36ad70f3
Mladen Marjanovic <mladen.marjanovic1992@gmail.com>
2017-03-07 21:18:05 +0000

    add user sign out

---
+++ spec/features/users/user_signout_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.feature "User sign out" do
+  
+  before do
+    @mladen = User.create!(email: "mladen@email.com", password: "password")
+    visit '/'
+    click_link "Sign in"
+    fill_in "Email", with: @mladen.email
+    fill_in "Password",  with: @mladen.password
+    click_button "Log in"
+  end
+  
+  scenario "user signs out" do
+    
+    visit "/"
+    click_link "Sign out"
+    
+    expect(page).to have_content("Signed out successfully")
+    
+  end
+  
+end
