5337bfd896f6e5e7c43557c36c8c280464ffa38c
Mladen Marjanovic <mladen.marjanovic1992@gmail.com>
2017-03-08 20:17:06 +0000

    invelid input exercise spec

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -27,4 +27,22 @@ RSpec.feature "Create exercise" do
     
   end
   
+  scenario "with valid input" do
+    visit "/"
+    
+    click_link "My Lounge"
+    click_link "New Workout"
+    expect(page).to have_link("Back")
+    
+    fill_in "Duration", with: ""
+    fill_in "Workout Details", with: ""
+    fill_in "Activity Date", with: ""
+    click_button "Create Exercise"
+    
+    expect(page).to have_content("Exercise has not been created")
+    expect(page).to have_content("Duration in min is not a number")
+    expect(page).to have_content("Workout details can't be blank")
+    expect(page).to have_content("Activity date can't be blank")
+  end
+  
 end
