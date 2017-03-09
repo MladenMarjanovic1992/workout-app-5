41b389590871e6a014a3012cb2c44a58c59356b3
Mladen Marjanovic <mladen.marjanovic1992@gmail.com>
2017-03-09 17:54:45 +0000

    listing user's workouts in lounge

---
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.feature "Listing Exercises" do
+  
+  before do
+    @mladen = User.create(email: "mladen@email.com", password: "password")
+    login_as(@mladen)
+    
+    @e1 = @mladen.exercises.new(duration_in_min: 50, workout: "running", workout_date: Date.today)
+    @e2 = @mladen.exercises.new(duration_in_min: 60, workout: "cardio", workout_date: 2.days.ago)
+  end
+  
+  scenario "shows user workouts for last 7 days" do
+    
+    visit "/"
+    click_link "My Lounge"
+    
+    expect(page).to have_content(@e1.duration_in_min)
+    expect(page).to have_content(@e1.workout)
+    expect(page).to have_content(@e1.workout_date)
+    
+    expect(page).to have_content(@e2.duration_in_min)
+    expect(page).to have_content(@e2.workout)
+    expect(page).to have_content(@e2.workout_date)
+    
+  end
+  
+end
