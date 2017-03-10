fa9b12cb7e14930d7da30f8afc5daf071f873871
Mladen Marjanovic <mladen.marjanovic1992@gmail.com>
2017-03-10 19:26:23 +0000

    implement edit exercise

---
+++ spec/features/exercises/editing_exercise_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.feature "Editing exercise" do
+  
+  before do
+    @mladen = User.create!(email: "mladen@email.com", password: "password")
+    
+    @e1 = @mladen.exercises.create!(duration_in_min: 50, workout: "running", workout_date: Date.today)
+    
+    login_as(@mladen)
+  end
+  
+  scenario "with valid input" do
+    visit "/"
+    
+    click_link "My Lounge"
+    
+    path = "/users/#{@mladen.id}/exercises/#{@e1.id}/edit"
+    link = "a[href=\'#{path}\']"
+    
+    find(link).click
+    
+    fill_in "Duration", with: 45
+    click_button "Update Exercise"
+    
+    expect(page).to have_content("Exercise has been updated")
+    expect(page).to have_content(45)
+    expect(page).not_to have_content(48)
+  end
+
+  
+end
