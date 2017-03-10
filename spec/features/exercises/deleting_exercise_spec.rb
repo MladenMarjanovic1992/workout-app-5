38d20f23c2f577a1310275c4b7bd36620a28183f
Mladen Marjanovic <mladen.marjanovic1992@gmail.com>
2017-03-10 19:37:36 +0000

    write delete exercise spec

---
+++ spec/features/exercises/deleting_exercise_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.feature "Delete exercise" do
+  
+  before do
+    @mladen = User.create(email: "mladen@email.com", password: "password")
+    login_as(@mladen)
+    
+    @e1 = @mladen.exercises.new(duration_in_min: 50, workout: "running", workout_date: Date.today)
+  end
+  
+  scenario do
+    
+    visit "/"
+    click_link "My Lounge"
+    
+    path = "/users/#{@mladen.id}/exercises/#{@e1.id}"
+    link = "//a[contains(@href,\'#{path}\') and .//text()='Destroy']"
+    
+    find(:xpath, link).click
+    
+    expect(page).to have_content("Exercise has been deleted")
+    
+  end
+  
+end
